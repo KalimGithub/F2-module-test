96f44af80708d8df69d3c5320af5caebd2017e1a
KalimGithub <sk22kalim@gmail.com>
2023-09-03 20:41:09 +0530

    first commit

--- script.js
+++ script.js
@@ -1,91 +1,15 @@
-// window.addEventListener("DOMContentLoaded", () => {
-
-//     document.getElementById("button").addEventListener("click", () => {
-//         document.getElementById("button").className = 'loading';
-
-//         const error = document.querySelector(".container p");
-
-//         // timezone
-//         const timeZoneString = () => {
-
-//         }
-//         // wind direction
-//         function windDirection = () => {
-
-//         }
-
-
-//         // getting location
-//         if ("geolocation" in navigator) {
-//             const userLocation = () => {
-//                 console.log("fetching results");
-
-//                 navigator.geolocation.getCurrentPosition(function (position) {
-//                     const latitude = position.coords.latitude;
-//                     const longitude = position.coords.longitude;
-
-
-//                     document.getElementById("latitude").textContent = `Lat: ${latitude}`;
-//                     document.getElementById("longitude").textContent = `Long: ${longitude}`;
-
-//                     // map using iframe
-//                     const iframe = document.createElement("iframe");
-//                     iframe.style.cssText = "height:100%;width: 100%;border: 0;";
-//                     // iframe.frameBorder = "0";
-//                     iframe.src = `https://maps.google.com/maps?q=${latitude},${longitude}&output=embed`;
-
-//                     document.querySelector(".map").appendChild(iframe);
-
-//                     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=5b4401f9c5767a1d4a6beb4b8757d980
-//                     `)
-//                         .then(e => e.json())
-//                         .then(data => {
-
-
-
-//                             document.getElementById("location").textContent = data.name;
-//                             document.getElementById("wind-speed").textContent = (data.wind.speed * 3.6) + "kmph";
-//                             document.getElementById("humidity").textContent = data.main.humidity;
-//                             document.getElementById("wind-direction").textContent = data.windDirection(data.wind.deg);
-//                             document.getElementById("time-zone").textContent = timeZoneString(data.timezone);
-//                             document.getElementById("pressure").textContent = data.data.main.pressure;
-//                             document.getElementById("uv-index");
-//                             document.getElementById("feels-like").textContent = data.main.feels_like;
-//                         })
-//                         .catch(e => {
-//                             error.textContent = e.message;
-//                         });
-//                 });
-//             }
-//             userLocation();
-//         } else {
-//             error.textContent = "Geolocation is not supported by your browser.";
-//         }
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const apiKey = "5b4401f9c5767a1d4a6beb4b8757d980";
+const apiKey = "3f68635044b9d3f6f30d0b1a5ddf2aa9";
 const fetchButton = document.getElementById("button");
 const map = document.getElementById("map");
 const homePage = document.getElementById("container-hidden");
 
 const container = document.getElementById("container");
-// const currentLocation = document.getElementById("location");
 const detail = document.getElementById("weather-details");
 const latitudeLongitude = document.getElementById("latitude-longitude");
+
+document.addEventListener('DOMContentLoaded', function() {
+    container.style.display = 'none';
+})
 
 function initMap(latitude, longitude) {
     map.src = `https://maps.google.com/maps?q=${latitude}, ${longitude}&z=18&output=embed`;
@@ -97,12 +21,10 @@ async function currentWeather(latitude, longitude) {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
     try {
-        const response = await fetch(url);
-        const weather = await response.json();
-
+        const result = await fetch(url);
+        const weather = await result.json();
         displayData(weather);
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 }
@@ -158,7 +80,7 @@ function displayData(weather) {
 }
 
 function fetchLocation() {
-    if ("Geolocation" in navigator) {
+    if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition
         (function (position) {
             const latitude = position.coords.latitude;
@@ -169,7 +91,8 @@ function fetchLocation() {
             
             currentWeather(latitude, longitude);
         });
-    } else {
+    }
+     else {
         alert("Geolocation is not supported in this browser.");
     }
 }
